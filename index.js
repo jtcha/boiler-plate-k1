262bfab74094fce70d6d4743bdbd246f7742d30f
jtcha <cielr47@gmail.com>
2021-01-26 15:39:19 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://cielr47:ckwlsrn1@cluster0.riqxf.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('mongodb connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log(`Example app lstening on port ${port}!`))
